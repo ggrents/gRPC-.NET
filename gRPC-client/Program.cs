541268fd19fb8e83a4269fe3260c8d9a371c2342
ggrents <chef_grents@mail.ru>
2024-02-04 13:47:17 +0600

    first service and client

---
+++ gRPC-client/Program.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Net.Client;
+using userManagement_client;
+
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5124");
+// создаем клиент
+var client = new Greeter.GreeterClient(channel);
+Console.Write("Введите имя: ");
+string? name = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name });
+Console.WriteLine($"Ответ сервера: {reply.Message}");
+Console.ReadKey();
